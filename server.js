d67791f7941b10d279506e9e13fd11ec90ff74b8
gwindham <chickenlord129@gmail.com>
2017-07-29 14:08:14 -0400

    garretts work

--- server.js
+++ server.js
@@ -26,4 +26,18 @@ MongoClient.connect('mongodb://testaccount:gennxt@ds113680.mlab.com:13680/gennxt
     console.log('listening on 3000')
   })
 })
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+app.get('/', (req, res) => {
+  var cursor = db.collection('quotes').find()
+})
+db.collection('quotes').find().toArray(function(err, results) {
+  console.log(results)
+  // send HTML file populated with quotes here
+})  
